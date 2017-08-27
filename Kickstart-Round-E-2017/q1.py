a402c2f1ce9c0a059918d8eb4c7ba04e82322f32
Max Pan <maxpan@google.com>
2017-08-28 01:26:37 +0800

    add solution

---
+++ Kickstart-Round-E-2017/q1.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+def solve(s):
+	length = len(s)
+	dp = [[None]*(length//2+1) for _ in range(length)]
+	dp[0][0] = 1
+	dpMin = [1]*length
+	lastOccurence = defaultdict(list)
+	for i in range(1, length):
+		dp[i][0] = dpMin[i-1] + 1
+		for j in range(2, i+2):
+			copied = s[i-j+1:i+1]
+			if j <= (i+1)//2:
+				lastOccurenceIndex = None
+				for index in reversed(lastOccurence[copied]):
+					if index <= i-j:
+						lastOccurenceIndex = index
+						break
+				if lastOccurenceIndex:
+					dp[i][j] = dpMin[i-j] + 2 #copy and paste
+					if dp[lastOccurenceIndex][j] != None:
+						dp[i][j] = min(dp[i][j], dp[lastOccurenceIndex][j]+(i-j-lastOccurenceIndex)+1)
+			lastOccurence[copied].append(i)
+		dpMin[i] = min([n for n in dp[i] if n != None])
+	return dpMin[-1]
+
+with open('q1.in', 'r') as inputFile:
+	lines = inputFile.readlines()
+
+n = int(lines[0])
+results = [None]*n
+for i, s in enumerate(lines[1:]):
+	results[i] = "Case #" + str(i+1) + ": " + str(solve(s[:-1])) + "\n"
+
+with open('q1.out', 'w') as outputFile:
+	outputFile.writelines(results)
